e343f2c703989c92e2e7d8e3d5fe2c1ed9afe04b
Javi GG <s.javi.com@gmail.com>
2020-09-07 10:58:05 +0200

    PNE metric

--- ISOLLE/ISOLLE.py
+++ ISOLLE/ISOLLE.py
@@ -239,6 +239,27 @@ class ISOLLE:
         y = np.array(y)
         return y
     
+    ### Metrics
+    def PNE(self):
+        '''
+        Valencia-Aguirre et al. Global and local choice of the number of nearest neighbors in locally linear embedding
+        Pattern Recognition Letters, Volume 32, Issue 16, (2011), Pages 2171-2177
+        https://www.sciencedirect.com/science/article/pii/S0167865511001553
+        '''
+        # Nearest neighbours in X
+        dist_matrix_X = pairwise_distances(self.X, metric = 'seuclidean')
+        knn_matrix_X = np.argsort(dist_matrix_X, axis = 1)[:, 1 : self.k_n + 1]
+        # Nearest neighbours in Y
+        dist_matrix_Y = pairwise_distances(self.Y, metric = 'seuclidean')
+        knn_matrix_Y = np.argsort(dist_matrix_Y, axis = 1)[:, 1 : self.k_n + 1]
+        # Neighbours in Y that aren't neigbhours in X
+        alpha = [[e for e in knn_matrix_Y[i] if e not in knn_matrix_X[i]] for i in range(self.n)]
+        
+        sum1 = sum( sum((dist_matrix_X[i,j] - dist_matrix_Y[i,j])**2 for j in knn_matrix_X[i])/self.k_n for i in range(self.n))
+        sum2 = sum( sum((dist_matrix_X[i,j] - dist_matrix_Y[i,j])**2 for j in alpha[i])/len(alpha[i]) for i in range(self.n) if len(alpha[i]) > 0)
+        
+        return np.round((sum1 + sum2)/(2*self.n), 3)
+    
     def plot_embedding_2d(self, colors, grid = True, dim_1 = 1, dim_2 = 2, cmap = None, size = (15, 10)):
         if self.dim < 2 and dim_2 <= self.dim and dim_1 <= self.dim:
             warnings.warn("There's not enough coordinates")
